cc912820721033ca1b2ccc6c5aad9fd6096f7673
Alexxmfs <alexxmfs@hotmail.com>
2022-06-11 15:22:48 -0300

    nft

--- nft/components/FocusedStatusBar.js
+++ nft/components/FocusedStatusBar.js
@@ -1,12 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StatusBar } from 'react-native'
+import { useIsFocused } from '@react-navigation/core';
 
-const FocusedStatusBar = () => {
-  return (
-    <View>
-      <Text>FocusedStatusBar</Text>
-    </View>
-  )
+const FocusedStatusBar = (props) => {
+  const isFocused = useIsFocused();
+
+  return isFocused ? <StatusBar animated={true} {...props} /> : null;
 }
 
 export default FocusedStatusBar
--- nft/components/NFTCard.js
+++ nft/components/NFTCard.js
@@ -1,9 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const NFTCard = () => {
+import { COLORS, SIZES, SHADOWS, assets } from '../constants';
+
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+
   return (
-    <View>
+    <View style={{ 
+      backgroundColor: COLORS.white,
+      borderRadius: SIZES.font,
+      marginBottom: SIZES.extraLarge,
+      margin: SIZES.base,
+      ...SHADOWS.dark
+      }}>
+        <View style={{ width: "100%", height: 250 }}>
+          <Image 
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%", 
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopLeftRadius: SIZES.font,
+          }}
+          />
+        </View>
+
       <Text>NFTCard</Text>
     </View>
   )
