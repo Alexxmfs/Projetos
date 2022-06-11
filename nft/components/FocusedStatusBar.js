1a697708d00d15f3d5c229a2f0005ee96a041e5e
Alexxmfs <alexxmfs@hotmail.com>
2022-06-10 22:12:31 -0300

    nft

---
+++ nft/components/FocusedStatusBar.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FocusedStatusBar = () => {
+  return (
+    <View>
+      <Text>FocusedStatusBar</Text>
+    </View>
+  )
+}
+
+export default FocusedStatusBar
--- nft/screens/Home.js
+++ nft/screens/Home.js
@@ -1,8 +1,14 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { View, SafeAreaView, FlatList, Text } from 'react-native';
+
+import { COLORS, NFTData } from '../constants';
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../components';
 
 const Home = () => {
   return (
-    <Text>Home</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+
+    </SafeAreaView>
     )
 }
 
