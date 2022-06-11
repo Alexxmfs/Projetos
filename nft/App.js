c2b9f946af9312795e16cd3cb9900ef7371925e3
Alexxmfs <alexxmfs@hotmail.com>
2022-06-10 21:15:31 -0300

    nft

---
+++ nft/App.js
@@ -0,0 +1,36 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import 'react-native-gesture-handler';
+
+
+import Home from "./screens/Home";
+import Details from "./screens/Details";
+
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent"
+  }
+}
+
+const App = () => {
+  const [loaded] = useFonts({
+    
+  })
+
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}
+      initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ nft/screens/Home.js
@@ -0,0 +1,9 @@
+import { Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <Text>Home</Text>
+    )
+}
+
+export default Home
