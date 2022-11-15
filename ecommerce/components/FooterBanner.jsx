e8f0fb861cc90a26cfe689e2862c685cb298a1ac
Alexxmfs <alexxmfs@hotmail.com>
2022-11-15 10:46:28 -0300

    QUACK

---
+++ ecommerce/components/FooterBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterBanner = () => {
+  return (
+    <div>
+      FooterBanner
+    </div>
+  )
+}
+
+export default FooterBanner
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
+import { Product, FooterBanner, HeroBanner } from '../components';
+
 const Home = () => {
   return (
     <>
       HeroBanner
 
       <div className="products-heading">
-        <h2>Beset Selling Product</h2>
-        <p>Speakers of many variations</p>
+        <h2>Produtos Mais Vendidos</h2>
+        <p>Alto-falantes de muitas variações</p>
       </div>
 
-      <div>
+      <div className="products-container">
         {['Product 1', 'Product 2'].map(
           (product) => product)}
       </div>
