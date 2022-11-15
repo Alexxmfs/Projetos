a1bf7dc2d4e48a624bb3b7f75232b80a723cb376
Alexxmfs <alexxmfs@hotmail.com>
2022-11-15 15:29:31 -0300

    QUACK

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import '../styles/globals.css'
+import React from 'react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
-    <>
-      HeroBanner
+    <div>
+      <HeroBanner />
 
       <div className="products-heading">
         <h2>Produtos Mais Vendidos</h2>
@@ -13,13 +14,24 @@ const Home = () => {
       </div>
 
       <div className="products-container">
-        {['Product 1', 'Product 2'].map(
-          (product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
-      Footer
-    </>
+      <FooterBanner />
+    </div>
   )
+}
+
+export const getServerSideProps =  async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home;
