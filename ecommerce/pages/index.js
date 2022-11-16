daae12a8486125d8aa86a4f03231435486ea9521
Alexxmfs <alexxmfs@hotmail.com>
2022-11-16 16:55:50 -0300

    QUACK

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -3,24 +3,22 @@ import React from 'react'
 import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = ({ products, bannerData }) => {
-  return (
+const Home = ({ products, bannerData }) => (
     <div>
-      <HeroBanner />
-
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className="products-heading">
         <h2>Produtos Mais Vendidos</h2>
         <p>Alto-falantes de muitas variações</p>
       </div>
 
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
       <FooterBanner />
     </div>
-  )
-}
+  );
 
 export const getServerSideProps =  async () => {
   const query = '*[_type == "product"]';
