7aa272ba5f962c552810b922bc139fd31c6e4798
Alexxmfs <alexxmfs@hotmail.com>
2022-11-18 00:20:42 -0300

    QUACK

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Head from 'next/head';
+
+import Navbar from './Navbar';
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>Alex's Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+
+      </main>
+    </div>
   )
 }
 
